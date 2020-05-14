029660ae0055d3d4b3a076fe585f8f7cc83a200e
Kevin Jordan <k.dean.jordan@gmail.com>
2020-05-14 08:45:15 -0700

    start server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('Main')
+const express = require('express')
+
+
+const app = express()
+
+
+app.listen(8080, () => {
+    console.log('Feed Listening on 8080')
+})
+
+app.get('/health', (req, res) => {
+    res.send('Users be healthy !')
+})
