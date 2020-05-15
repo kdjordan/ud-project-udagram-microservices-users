0aa3856026240e77e74ebf1c71dbfb2d6d2d3150
Kevin Jordan <k.dean.jordan@gmail.com>
2020-05-15 08:52:39 -0700

    added ts support and user model

---
+++ server.ts
@@ -0,0 +1,47 @@
+const express = require('express')
+
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import {sequelize} from './sequelize';
+import { Request, Response } from 'express';
+import { User } from './models/User';
+import { AuthRouter, requireAuth } from './auth.router';
+
+(async() => {
+    const app = express()
+    const V0_USER_MODEL = [User];
+    await sequelize.addModels(V0_USER_MODEL);
+    require('dotenv').config()
+    
+    // app.use('/auth', AuthRouter);
+    
+    app.listen(8082, () => {
+        console.log('listening on 8082')
+    })
+
+    app.use(bodyParser.json());
+
+    app.use(cors({
+        allowedHeaders: [
+        'Origin', 'X-Requested-With',
+        'Content-Type', 'Accept',
+        'X-Access-Token', 'Authorization',
+        ],
+        methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+        // origin: config.url,
+        origin: 'http://lcoalhost:8080',
+    }));
+    
+    app.get('/', (req: Request, res: Response) => {
+        console.log('hitting')
+        res.send('USER FEED RUNNING')
+    });
+    
+    app.get('/:id', async (req: Request, res: Response) => {
+      const {id} = req.params;
+      const item = await User.findByPk(id);
+      res.send(item);
+    });
+})();
+
+
